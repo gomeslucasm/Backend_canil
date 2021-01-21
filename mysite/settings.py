f9a8e1eafdcc409ef24f8d42a42d5a4f8035dab0
Lucas Gomes <lmgomes96@gmail.com>
2021-01-20 22:58:18 -0300

    add django_heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-''' import django_heroku
 
-
-django_heroku.settings(locals()) '''
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -184,3 +181,6 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+import django_heroku
+django_heroku.settings(locals())
