dafecd349bb2a80b43540d67fdd4e30de5353ff3
Lucas Gomes <lmgomes96@gmail.com>
2021-02-04 16:27:36 -0300

    teste

--- mysite/settings.py
+++ mysite/settings.py
@@ -160,7 +160,7 @@ if DEBUG:
     ]
 else:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
